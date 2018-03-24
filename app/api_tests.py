075213e5a28e80bc2ed0a773fdfe2a52d9ea326c
Prince <=>
2018-03-24 14:26:54 +0300

    Refine unitests to ensure items are retrieved.

--- app/api_tests.py
+++ app/api_tests.py
@@ -13,12 +13,17 @@ class BookAPITests(unittest.TestCase):
 
     def tearDown(self):
         self.app.testing = False
+        self.app = None
 
     def test_get_all_books(self):
         # test for homepage
         resp = self.app.get(BASE_URL)
         self.assertEqual(resp.status_code, 200,
                          msg='Should retrieve data from the api.')
+
+        data = json.loads(resp.get_data().decode('utf-8'))
+        no_of_items = len(data)
+        self.assertTrue(no_of_items != 0, msg = 'Should retrieve items')
 
 
 if __name__ == '__main__':
--- app/resources/models.py
+++ app/resources/models.py
@@ -2,13 +2,12 @@ from flask_restful import Resource
 from flask import jsonify
 
 
-# A list to hold book itesms
-books = [{
-    'id': 1, 'title': 'Python Programming'
-}, {'id': 3, 'title': 'Java programming'},
+# A list for holding book itesms
+books = [
+    {'id': 1, 'title': 'Python Programming'}, {'id': 3, 'title': 'Java programming'}
 
 ]
 class Books(Resource):
 
     def get(self):
-        return jsonify({'Books': books})
+        return (books)
